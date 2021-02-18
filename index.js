86fcc6e3ba461556a7523c3624686e9bbe74aa03
Ved Javle <vvjavle@gmail.com>
2021-02-18 15:03:54 -0500

    Created index.js
    Created a function to deploy in Azure - displays the current time and date using JavaScript

---
+++ index.js
@@ -0,0 +1,23 @@
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    const name = (req.query.name || (req.body && req.body.name));
+
+    var currentdate = new Date(); 
+    var datetime = "Last Sync: " + (currentdate.getMonth() + 1) + "/"
+                + currentdate.getDate() + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : currentdate + " " + datetime;
+        //: "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
